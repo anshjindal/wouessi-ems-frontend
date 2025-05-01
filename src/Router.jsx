b7a250dd4df20dc901c0d4d45a492f4414fb09da
Eric Njanga <eric.njanga@gmail.com>
2025-05-01 13:34:40 -0400

    Hiding unecessary routes and components

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AboutUs from "./pages/AboutUs/AboutUs";
-import ContactUs from "./pages/ContactUs/ContactUs";
-import Dashboard from "./pages/Dashboard/Dashboard";
-import EmployeeManagement from "./pages/EmployeeManagement/EmployeeManagement";
-import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
-import Login from "./pages/Login/Login";
-import Onboarding from "./pages/Onboarding/Onboarding";
 import Profile from "./pages/Profile/Profile";
 import TraineeRegistration from "./pages/TraineeRegistration/TraineeRegistration";
 import TraineeLogin from './pages/TraineeLogin/TraineeLogin';
-import DtsDashboardLayout from './pages/DtsDashboard/DtsDashboardLayout';
-import DashboardHome from './pages/DtsDashboard/Home';
-import Vetting from './pages/DtsDashboard/Vetting/Vetting';
 import VettingProcess from './pages/VettingProcess/VettingProcess';
 import StudentList from "./pages/StudentList/StudentList";
 import CohortManagement from "./pages/CohortManagement/CohortManagement"
@@ -22,21 +12,10 @@ const AppRouter = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/contact" element={<ContactUs />} />
-                <Route path="/about" element={<AboutUs />} />
-                <Route path="/employee-management" element={<EmployeeManagement />} />
-                <Route path="/onboarding" element={<Onboarding />} />
+                <Route path="/" element={<TraineeRegistration />} />  
+                <Route path="/profile" element={<Profile />} />   
                 <Route path="/trainee-registration" element={<TraineeRegistration />} />
-                <Route path="/trainee-login" element={<TraineeLogin />} />
-                <Route path="/dts-dashboard" element={<DtsDashboardLayout />}>
-                    <Route index element={<DashboardHome />} />
-                    <Route path="vetting" element={<Vetting />} />
-                </Route>
+                <Route path="/trainee-login" element={<TraineeLogin />} /> 
                 <Route path="/vetting-process" element={<VettingProcess/>} />
                 <Route path="/student-list" element={<StudentList/>} />
                 <Route path="/cohort-management" element={<CohortManagement/>} />
